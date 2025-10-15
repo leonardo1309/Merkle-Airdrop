b7da6f4b0c8f6f37205854abc20d0fc805142a8f
leonardo <leonardo.mu00@gmail.com>
2025-10-15 08:06:17 -0500

    -

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -17,30 +17,30 @@ contract MerkleAirdrop {
 
     event Claim(address indexed account, uint256 amount);
 
- constructor(bytes32 merkleRoot, IERC20 airdropToken) {
+    constructor(bytes32 merkleRoot, IERC20 airdropToken) {
         i_merkleRoot = merkleRoot;
         i_airdropToken = airdropToken;
- }   
-
- function claim(address account, uint256 amount, bytes32[] calldata merkleProof) external {
-    if(s_hasClaimed[account]) {
-        revert MerkleAirdrop__AlreadyClaimed();
     }
-    //Calculate uisng the account and amount, the hash -> leaf node
-    bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account, amount))));
-    if(!MerkleProof.verify(merkleProof, i_merkleRoot, leaf)){
-        revert MerkleAirdrop__InvalidProof();
+
+    function claim(address account, uint256 amount, bytes32[] calldata merkleProof) external {
+        if (s_hasClaimed[account]) {
+            revert MerkleAirdrop__AlreadyClaimed();
+        }
+        //Calculate uisng the account and amount, the hash -> leaf node
+        bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account, amount))));
+        if (!MerkleProof.verify(merkleProof, i_merkleRoot, leaf)) {
+            revert MerkleAirdrop__InvalidProof();
+        }
+        s_hasClaimed[account] = true;
+        emit Claim(account, amount);
+        i_airdropToken.safeTransfer(account, amount);
     }
-    s_hasClaimed[account] = true;
-    emit Claim(account, amount);
-    i_airdropToken.safeTransfer(account, amount);
- }
 
- function getMerkleRoot() external view returns(bytes32) {
-    return i_merkleRoot;
- }
+    function getMerkleRoot() external view returns (bytes32) {
+        return i_merkleRoot;
+    }
 
- function getAirdropToken() external view returns(IERC20) {
-    return i_airdropToken;
- }
+    function getAirdropToken() external view returns (IERC20) {
+        return i_airdropToken;
+    }
 }
