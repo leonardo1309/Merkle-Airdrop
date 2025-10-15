81cfac012cdc873730010548e5ce4292692aab21
leonardo <leonardo.mu00@gmail.com>
2025-10-14 20:18:22 -0500

    Airdrop first test success

--- src/BagelToken.sol
+++ src/BagelToken.sol
@@ -5,7 +5,10 @@ import {ERC20} from "openzeppelin-contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "openzeppelin-contracts/access/Ownable.sol";
 
 contract BagelToken is ERC20, Ownable{
-    constructor(address to, uint256 amount) ERC20("BagelToken", "BGL") Ownable(msg.sender) {
+    constructor() ERC20("BagelToken", "BGL") Ownable(msg.sender) {
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
     }
 }
